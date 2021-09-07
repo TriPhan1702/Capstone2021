7ca8ca65528af83d1ffb93007d9bd3961d392434
PhanMinhTri <tphan2883@gmail.com>
2021-09-07 17:53:25 +0700

    AddBaseController

---
+++ HairCutAPI/HairCutAPI/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HairCutAPI.Data;
+
+namespace HairCutAPI.Controllers
+{
+    public class UsersController : BaseApiController
+    {
+        private readonly HDBContext _context;
+
+        public UsersController(HDBContext _context)
+        {
+            this._context = _context;
+        }
+    }
+}
