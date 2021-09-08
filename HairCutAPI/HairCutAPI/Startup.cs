cbb58aca4540c253285e34575a615db5e1f317d4
PhanMinhTri <tphan2883@gmail.com>
2021-09-08 19:09:11 +0700

    Add Authorization to swagger

--- HairCutAPI/HairCutAPI/Startup.cs
+++ HairCutAPI/HairCutAPI/Startup.cs
@@ -33,9 +33,30 @@ namespace HairCutAPI
             services.AddApplicationServices(_config);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(option =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HairCutAPI", Version = "v1" });
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "HairCutAPI", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {{
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                }});
             });
 
             //Use cross origin service
