358c796348ed094d8c7e462c722143142d053235
PhanMinhTri <tphan2883@gmail.com>
2021-09-08 19:11:36 +0700

    Add seeding database

---
+++ HairCutAPI/HairCutAPI/Data/Seed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HairCutAPI.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace HairCutAPI.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            //Seed roles
+            var roles = new List<AppRole>()
+            {
+                new AppRole{Name = "Administrator"},
+                new AppRole{Name = "Manager"},
+                new AppRole{Name = "Employee"},
+                new AppRole{Name = "Customer"},
+                new AppRole{Name = "NewCustomer"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
--- HairCutAPI/HairCutAPI/Program.cs
+++ HairCutAPI/HairCutAPI/Program.cs
@@ -6,14 +6,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HairCutAPI.Data;
+using HairCutAPI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HairCutAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //Seed database
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<HDBContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(userManager, roleManager);
+            }
+            catch (Exception e)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "An Error occured during migration");
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
