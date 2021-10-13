f4e48c52760a1a9b779094972d07c826e1156f86
PhanMinhTri <tphan2883@gmail.com>
2021-10-13 21:57:11 +0700

    Make PaginationHeader Camel Case

--- HairCutAppAPI/Utilities/Extensions/HttpExtensions.cs
+++ HairCutAppAPI/Utilities/Extensions/HttpExtensions.cs
@@ -12,7 +12,13 @@ namespace HairCutAppAPI.Utilities.Extensions
             int totalItems, int totalPages)
         {
             var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
-            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            //Option to make pagination Header camel case
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
             response.Headers.Add("Access-Control-Expose-Headers","Pagination");
         }
     }
