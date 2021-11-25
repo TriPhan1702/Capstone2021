606736bbe95526a2eaf8044b9d442d2b16ba0461
PhanMinhTri <tphan2883@gmail.com>
2021-11-25 14:21:03 +0700

    Add Appointment Canceled Notification

--- HairCutAppAPI/Services/BackgroundJobService.cs
+++ HairCutAppAPI/Services/BackgroundJobService.cs
@@ -43,6 +43,7 @@ namespace HairCutAppAPI.Services
                     appointment.Status = GlobalVariables.CanceledAppointmentStatus;
                     appointment.LastUpdated = DateTime.Now;
                     await _repositoryWrapper.Appointment.UpdateAsyncWithoutSave(appointment, appointment.Id);
+                    await SendAppointmentCanceledNotifications(appointment);
                 }
 
                 foreach (var slot in workslots)
@@ -66,14 +67,11 @@ namespace HairCutAppAPI.Services
                 {
                     //Save all changes above to database 
                     await _repositoryWrapper.SaveAllAsync();
-                    
-                    //TODO: Send cancel notification
                 }
                 catch (Exception e)
                 {
                     //clear pending changes if fail
                     _repositoryWrapper.DeleteChanges();
-                    //TODO: Log failure to update appointment status
                 }
             }
         }
@@ -305,6 +303,85 @@ namespace HairCutAppAPI.Services
                 _repositoryWrapper.DeleteChanges();
                 //TODO: Log Failure
             }
+        }
+        
+        private async Task SendAppointmentCanceledNotifications(Appointment appointment)
+        {
+            //Prepare Customer's Notifications
+            var customer =
+                (await _repositoryWrapper.Customer.FindSingleByConditionAsync(cus => cus.Id == appointment.CustomerId));
+            if (customer != null)
+            {
+                    await _repositoryWrapper.Notification.CreateWithoutSaveAsync(ToNewNotification(
+                        appointment,
+                        "Buỗi hẹn của bạn đã bị hủy",
+                        $"Buỗi hẹn lúc {appointment.StartDate.ToString(GlobalVariables.DateTimeFormat)} đến {appointment.EndDate.ToString(GlobalVariables.DateTimeFormat)} của bạn đã bị hủy, click vào đây để xem chi tiết...",
+                        customer.UserId, GlobalVariables.AppointmentCanceledNotification));
+            }
+
+            var appointmentStaffUserIds =
+                (await _repositoryWrapper.AppointmentDetail.FindByConditionAsyncWithInclude(
+                    detail => detail.AppointmentId == appointment.Id, detail => detail.Staff))
+                .Select(detail => detail.Staff.UserId).ToHashSet();
+
+            foreach (var staffUserId in appointmentStaffUserIds)
+            {
+                await _repositoryWrapper.Notification.CreateWithoutSaveAsync(ToNewNotification(
+                    appointment,
+                    "Buỗi hẹn của bạn đã bị hủy",
+                    $"Buỗi hẹn lúc {appointment.StartDate.ToString(GlobalVariables.DateTimeFormat)} đến {appointment.EndDate.ToString(GlobalVariables.DateTimeFormat)} của khách hàng {customer.FullName} đã bị hủy do đã quá thời gian xac nhận",
+                    staffUserId, GlobalVariables.AppointmentCanceledNotification));
+            }
+            
+            var managers = (await _repositoryWrapper.Staff.FindByConditionAsync(staff =>
+                    staff.StaffType == GlobalVariables.ManagerRole &&
+                    staff.User.Status == GlobalVariables.ActiveUserStatus && staff.SalonId == appointment.SalonId))
+                .ToList();
+            foreach (var manager in managers)
+            {
+                await _repositoryWrapper.Notification.CreateWithoutSaveAsync(ToNewNotification(
+                    appointment,
+                    $"Buổi hẹn của {customer.FullName} đã bị hủy",
+                    $"Buỗi hẹn lúc {appointment.StartDate.ToString(GlobalVariables.DateTimeFormat)} đến {appointment.EndDate.ToString(GlobalVariables.DateTimeFormat)} của khách hàng {customer.FullName} đã bị hủy do đã quá thời gian xac nhận",
+                    manager.UserId , GlobalVariables.AppointmentCanceledNotification));
+            }
+
+            var reminderNotifications = await _repositoryWrapper.Notification.FindByConditionAsync(notification =>
+                notification.Type == GlobalVariables.AppointmentReminderNotification &&
+                notification.AppointmentId == appointment.Id &&
+                notification.Status == GlobalVariables.PendingNotificationStatus);
+            foreach (var notification in reminderNotifications)
+            {
+                notification.Status = GlobalVariables.CanceledNotificationStatus;
+                await _repositoryWrapper.Notification.UpdateAsyncWithoutSave(notification, notification.Id);
+            }
+
+            try
+            {
+                //Save all changes above to database 
+                await _repositoryWrapper.SaveAllAsync();
+            }
+            catch (Exception e)
+            {
+                //clear pending changes if fail
+                _repositoryWrapper.DeleteChanges();
+            }
+        }
+        
+        private Notification ToNewNotification(Appointment appointment, string title, string detail,
+            int userId, string type)
+        {
+            return new Notification()
+            {
+                Detail = detail,
+                Status = GlobalVariables.PendingNotificationStatus,
+                Title = title,
+                Type = type,
+                AppointmentId = appointment.Id,
+                UserId = userId,
+                CreatedDate = DateTime.Now,
+                LastUpdate = DateTime.Now,
+            };
         }
     }
 }
